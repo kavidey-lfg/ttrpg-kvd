baee2678776b32704e837d913f1427ab2c938f2c
kavidey-lfg <clifford4gunawan.soc@gmail.com>
2026-04-16 20:34:17 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-export default defineConfig({
-  plugins: [react()],
-})
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import {defineConfig, loadEnv} from 'vite';
+
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, '.', '');
+  return {
+    plugins: [react(), tailwindcss()],,
+  };
+});
