e0e36fa7831e1f96f62351d9711087432eaac85d
kavidey-lfg <clifford4gunawan.soc@gmail.com>
2026-04-16 20:36:00 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    plugins: [react(), tailwindcss()],,
+    plugins: [react(), tailwindcss()],
   };
 });
